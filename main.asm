ceb5d4b6e560d156ed63d8a8344e7dfee9a8d5cd
HyperX98 <raulxiloj98@gmail.com>
2020-03-29 21:06:22 -0600

    Se guardan coeficientes

--- main.asm
+++ main.asm
@@ -4,11 +4,22 @@ include macros.asm
 .stack 100h
 ;-----Data segment-----
 .data
-header    db "UNIVERSIDAD DE SAN CARLOS DE GUATEMALA",10,13,"FACULTAD DE INGENIERIA",10,13,"CIENCIAS Y SISTEMAS",10,13,"ARQUITECTURA DE COMPUTADORAS Y ENSAMBLADORES 1",10,13,"NOMBRE: RAUL XILOJ",10,13,"CARNET: 201612113",10,13,"SECCION: A",10,10,13,'$'
-options   db "1. Ingresar funcion f(x)",10,13,"2. Funcion en memoria",10,13,"3. Derivada f'(x)",10,13,"4. Integral F(x)",10,13,"5. Graficar funciones",10,13,"6. Reporte",10,13,"7. Modo calculadora",10,13,"8. Salir",10,13,10,13,"Ingrese una opcion: ",'$'
-prueba    db 10,13,"Esto es una prueba",10,10,13,'$'
+header      db "UNIVERSIDAD DE SAN CARLOS DE GUATEMALA",10,13,"FACULTAD DE INGENIERIA",10,13,"CIENCIAS Y SISTEMAS",10,13,"ARQUITECTURA DE COMPUTADORAS Y ENSAMBLADORES 1",10,13,"NOMBRE: RAUL XILOJ",10,13,"CARNET: 201612113",10,13,"SECCION: A",10,10,13,'$'
+options     db "1. Ingresar funcion f(x)",10,13,"2. Funcion en memoria",10,13,"3. Derivada f(x)",10,13,"4. Integral F(x)",10,13,"5. Graficar funciones",10,13,"6. Reporte",10,13,"7. Modo calculadora",10,13,"8. Salir",10,13,10,13,"Ingrese una opcion: ",'$'
+coefX       db "- Coeficiente de xx: ",'$'
+co4         db 5 dup ('$')
+co3         db 5 dup ('$')
+co2         db 5 dup ('$')
+co1         db 5 dup ('$')
+co0         db 5 dup ('$')
+number      db 2 dup (0)
+newLine     db 10,'$'
+prueba      db 10,13,"Esto es una prueba gg",10,13,'$'
 ;--------------------------Possibles errors:--------------------------
-error1    db 10,10,13,"Error: opcion invalida",10,10,13,'$'
+error1   db 10,10,13,"Error: Opcion invalida",10,10,13,'$'
+error2   db 10,10,13,"Error: No se acepta caracteres vacios",10,10,13,'$'
+error3   db 10,10,13,"Error: Solo se aceptan digitos entre -9 a +9",10,10,13,'$'
+error4   db 10,10,13,"Error: caracter no reconocido",10,10,13,'$'
 ;----------------------------------------------------------------------------------------------
 ;-----------------------------------------Code segment-----------------------------------------
 ;----------------------------------------------------------------------------------------------
@@ -16,8 +27,8 @@ error1    db 10,10,13,"Error: opcion invalida",10,10,13,'$'
 main proc
     mov ax, @data
     mov ds,ax
+    print header
     menuPrincipal:
-        print header
         print options
         getChar 
         cmp al, '1'
@@ -38,13 +49,20 @@ main proc
         je exit
         jmp invalidOption
     insertFunction:
-        print prueba
+        print newLine
+        getCoefficients
         jmp menuPrincipal
     memoryFunction:
-        print prueba
+        ;TODO printFunction
         jmp menuPrincipal
     derivative:
-        print prueba
+        print newLine
+        getTexto co0
+        print newLine
+        convertNumber co0, number
+        convertAscii number, co1
+        print co1
+        print newLine
         jmp menuPrincipal
     integral:
         print prueba
@@ -61,9 +79,12 @@ main proc
     exit:
         mov ah, 4ch
         int 21h
-
     invalidOption:
         print error1
         jmp menuPrincipal
+    errorChar: 
+        print error4
+        jmp menuPrincipal
 main endp
+
 end main
