f8a83eb07ed2051652ef6cc7f739d23161da877b
HyperX98 <raulxiloj98@gmail.com>
2020-04-02 14:39:49 -0600

    Macros para pasar a postfijo y verificar precedencia

--- analisis.asm
+++ analisis.asm
@@ -104,3 +104,123 @@ LOCAL while, finish, continue, symbol, continue, error, space
         finish:
 endm
 
+toPostfix macro
+LOCAL while, continue, finish, plus, minus
+    mov stackPost, 0
+    xor si, si
+    xor cx, cx  ;sizeOf postfix string
+    xor bx, bx
+
+    while:
+        cmp si, fileSize
+        jge finish
+
+        mov bl, fileData[si]
+        ;space
+        cmp bl, 32
+        je continue
+        ;symbols
+        cmp bl, '+'
+        je plus
+        cmp bl, '-'
+        je minus
+        cmp bl, '*'
+        je times
+        cmp cl, '/'
+        je divs
+
+        ;mov num to postfix
+        push si
+        mov si, cx
+        mov postfix[si],bl
+        pop si
+        inc cx
+        jmp continue
+
+        plus:
+            precedencia 1, '+'
+            jmp continue
+
+        minus:
+            precedencia 1, '-'
+            jmp continue
+
+        times:
+            precedencia 2, '*'
+            jmp continue
+
+        divs:
+            precedencia 2, '/'
+            jmp continue
+
+        continue:
+            inc si
+            jmp while
+        
+        finish:
+            vaciarPila
+            print newLine
+            print postfix
+endm
+
+precedencia macro op, sym
+LOCAL isEmpty, equal, greater, lower, finish
+    int 3
+    cmp stackPost, 0
+    je isEmpty
+
+    pop ax
+    cmp al, op
+    je equal
+    cmp al, op
+    jl greater
+    jmp lower
+
+    equal:;When is equal it's replaced 
+        push si
+        mov si, cx
+        mov postfix[si], ah
+        pop si
+
+        mov al, op
+        mov ah, sym
+        push ax
+        inc cx
+        jmp finish
+
+    greater:;When is greater it's just added to the 'stack'
+        inc stackPost
+        push ax
+        mov al, op
+        mov ah, sym
+        push ax
+        jmp finish
+    
+    lower:;When is lower it empty the stack 
+        push ax
+        vaciarPila
+
+    isEmpty:
+        inc stackPost
+        mov al, op
+        mov ah, sym
+        push ax
+    
+    finish:
+endm
+
+vaciarPila macro
+LOCAL while, finish
+    while:
+        cmp stackPost, 0
+        je finish
+        pop bx
+        push si
+        mov si, cx
+        mov postfix[si], bh
+        pop si
+        inc cx
+        dec stackPost
+        jmp while
+    finish:
+endm
