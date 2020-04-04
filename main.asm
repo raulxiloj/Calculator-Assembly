96e564492ece555dc5c7ff810a1604628e7c7109
HyperX98 <raulxiloj98@gmail.com>
2020-04-04 01:46:14 -0600

    Se agregaron variables y se ordenaron para llevar un mejor control

--- main.asm
+++ main.asm
@@ -2,60 +2,93 @@ include macros.asm
 include graphics.asm
 include files.asm
 include analisis.asm
+include reporte.asm
+include date.asm
 .model small
 ;-----Stack segment-----
 .stack 100h
 ;-----Data segment-----
 .data
-header    db "UNIVERSIDAD DE SAN CARLOS DE GUATEMALA",10,13,"FACULTAD DE INGENIERIA",10,13,"CIENCIAS Y SISTEMAS",10,13,"ARQUITECTURA DE COMPUTADORAS Y ENSAMBLADORES 1",10,13,"NOMBRE: RAUL XILOJ",10,13,"CARNET: 201612113",10,13,"SECCION: A",10,'$'
-options   db 10,13,"1. Ingresar funcion f(x)",10,13,"2. Funcion en memoria",10,13,"3. Derivada f(x)",10,13,"4. Integral F(x)",10,13,"5. Graficar funciones",10,13,"6. Reporte",10,13,"7. Modo calculadora",10,13,"8. Salir",10,13,10,13,"Ingrese una opcion: ",'$'
-graphMenu db 10,10,13,"1. Graficar original f(x)",10,13,"2. Graficar derivada f(x)",10,13,"3. Graficar integral F(x)",10,13,"4. Regresar",10,13,10,13,"Ingrese una opcion: ",'$'
-RangeInit db 10,10,13, "Ingrese el valor inicial del intervalo: ",'$'
-RangeEnd  db "Ingrese el valor final del intervalo: ",'$'
-inter1    db 5 dup ('$')
-inter2    db 5 dup ('$')
-auxInter  db 5 dup ('$')
-coefX     db "- Coeficiente de xx: ",'$'
-fx        db 10,10,"       f(x) =",'$'
-plus      db " + ",'$'
-minus     db " - ",'$'
-varX      db "xx",'$'
-c4        db 5 dup ('$')
-c3        db 5 dup ('$')
-c2        db 5 dup ('$')
-c1        db 5 dup ('$')
-c0        db 5 dup ('$')
-d4        db ?
-d3        db ?
-d2        db ?
-d1        db ?
-auxCo     db "    ",'$' 
-deriv     db 5 dup ('$')
-number    db ?
-newLine   db 10,'$'
-postfix   db 1000 dup('$')
-stackPost db 0
-prueba    db 10,13,"Esto es una prueba gg",10,13,'$'
-;---------------------File messages and variables---------------------
-file1     db 50 dup('$')    
-file2     db 50 dup('$')  
-fileSize  dw ? 
-handler   dw ? 
-fileData  db 1000 dup('$')
-inputFile db 10,13,"Ingrese la ruta del archivo: ",'$'
-reading   db "Leyendo el archivo...",'$'
-;--------------------------Possibles errors:--------------------------
-error1   db 10,13,"Error: Opcion invalida",10,13,'$'
-error2   db 10,13,"Error: No se acepta caracteres vacios",10,13,'$'
-error3   db 10,13,"Error: Solo se aceptan digitos entre -9 a +9",10,13,'$'
-error4   db 10,13,"Error: caracter no reconocido",10,13,'$'
-error5   db 10,13,"Error al abrir archivo",10,13,'$'
-error7   db 10,13,"Error al escribir en el archivo",10,13,'$'
-error8   db 10,13,"Error al crear el archivo",10,13,'$'
-error9   db 10,13,"Error al leer el archivo",10,13,'$'
-error10  db 10,13,"Error al cerrar el archivo",10,13,'$'
-error11  db 10,13,"Error: caracter no reconocido en el archivo",10,13,'$'
-error12  db 10,13,"Error: no se esperaba ese caracter en esa posicion", 10,13,'$'
+header       db "UNIVERSIDAD DE SAN CARLOS DE GUATEMALA",10,"FACULTAD DE INGENIERIA",10,"CIENCIAS Y SISTEMAS",10,"ARQUITECTURA DE COMPUTADORAS Y ENSAMBLADORES 1",10,"NOMBRE: RAUL XILOJ",10,"CARNET: 201612113",10,"SECCION: A",10,'$'
+options      db 10,13,"1. Ingresar funcion f(x)",10,13,"2. Funcion en memoria",10,13,"3. Derivada f(x)",10,13,"4. Integral F(x)",10,13,"5. Graficar funciones",10,13,"6. Reporte",10,13,"7. Modo calculadora",10,13,"8. Salir",10,13,10,13,"Ingrese una opcion: ",'$'
+graphMenu    db 10,10,13,"1. Graficar original f(x)",10,13,"2. Graficar derivada f(x)",10,13,"3. Graficar integral F(x)",10,13,"4. Regresar",10,13,10,13,"Ingrese una opcion: ",'$'
+inter1       db 5 dup ('$')
+inter2       db 5 dup ('$')
+auxInter     db 5 dup ('$')
+coefX        db "- Coeficiente de xx: ",'$'
+fx           db 10,10,"       f(x) =",'$'
+plus         db " + ",'$'
+minus        db " - ",'$'
+fraction     db "/",'$'
+varX         db "xx",'$'
+space        db " ",'$'
+constantI    db " + C ",10,'$'
+;---------------------------------Function-----------------------------------
+c4           db 5 dup ('$')
+c3           db 5 dup ('$')
+c2           db 5 dup ('$')
+c1           db 5 dup ('$')
+c0           db 5 dup ('$')
+;--------------------------------Derivada-----------------------------------
+d3           db 2 dup (0)
+d2           db 2 dup (0)
+d1           db 2 dup (0)
+d0           db 2 dup (0)
+;--------------------------------Integral-----------------------------------
+i5           db 3 dup (0)
+i4           db 3 dup (0)
+i3           db 3 dup (0) 
+i2           db 3 dup (0)
+i1           db 3 dup (0)
+;---------------------------------------------------------------------------
+number       db ?
+auxCo        db 0,'$'
+deriv        db 3 dup ('0'),'$'
+newLine      db 10,'$'
+res          db 5 dup ('$')
+postfix      db 1000 dup('$')
+stackPost    db 0
+;--------------------------------Reporte-------------------------------------
+repAddr      db "c:\p5\reporte.txt",0
+repMsg1      db 10,10,9,9,9,9,"Reporte practica no. 5",10,9,9,9,9,"----------------------",10,10
+repMsg2      db "Funcion original:",10,"----------------"
+repMsg3      db "Funcion derivada:",10,"----------------"
+repMsg4      db "Funcion integral:",10,"----------------"
+date         db 10,"Fecha: dd/mm/2020  "
+time         db 10,"Hora:  00:00:00" 
+repSuccess   db 10,"Reporte generado con exito",10,'$'
+;----------------------------------Messages----------------------------------
+msgInput1    db 10,"Ingrese los coeficientes:",10,10,'$' 
+msgInput2    db 10,"Ingrese la ruta del archivo: ",'$'
+msgInput3    db 10,10,13, "Ingrese el valor inicial del intervalo: ",'$'
+msgInput4    db "Ingrese el valor final del intervalo: ",'$'
+msgOp        db 10,"Operacion: ",'$'
+msgRes       db 10,"Resultado: ",'$'
+msgFunction  db 10,"Funcion en memoria: ",'$'
+msgDerived   db 10,"Derivada de la funcion: ", '$'
+msgIntegral  db 10,"Integral de la funcion: ", '$'
+;-----------------------------File variables---------------------------------
+file1        db 50 dup('$')    
+file2        db 50 dup('$')
+fileSize     dw ? 
+handler      dw ? 
+fileData     db 1000 dup('$')
+reading      db "Leyendo el archivo...",'$'
+;----------------------------Possibles errors--------------------------------
+error1       db 10,13,"Error: Opcion invalida",10,13,'$'
+error2       db 10,13,"Error: No se acepta caracteres vacios",10,13,'$'
+error3       db 10,13,"Error: Solo se aceptan digitos entre -9 a +9",10,13,'$'
+error4       db 10,13,"Error: caracter no reconocido",10,13,'$'
+error5       db 10,13,"Error al abrir archivo",10,13,'$'
+error7       db 10,13,"Error al escribir en el archivo",10,13,'$'
+error8       db 10,13,"Error al crear el archivo",10,13,'$'
+error9       db 10,13,"Error al leer el archivo",10,13,'$'
+error10      db 10,13,"Error al cerrar el archivo",10,13,'$'
+error11      db 10,13,"Error: caracter no reconocido en el archivo",10,13,'$'
+error12      db 10,13,"Error: no se esperaba ese caracter en esa posicion", 10,13,'$'
+error13      db 10,13,"Error: se esperaba ',59,' al final de la cadena",10,13,'$'
+error14      db 10,13,"Error: moviendo el puntero del fichero",10,13,'$'
+prueba       db 10,13,"Esto es una prueba gg",10,13,'$'
 ;----------------------------------------------------------------------------------------------
 ;-----------------------------------------Code segment-----------------------------------------
 ;----------------------------------------------------------------------------------------------
@@ -63,6 +96,8 @@ error12  db 10,13,"Error: no se esperaba ese caracter en esa posicion", 10,13,'$
 main proc
     mov ax, @data
     mov ds,ax
+    clearScreen
+    print newLine
     print header
     menuPrincipal:
         print options
@@ -85,34 +120,39 @@ main proc
         je exit
         jmp invalidOption
     insertFunction:
-        print newLine
+        clearScreen
         getCoefficients
         jmp menuPrincipal
     memoryFunction:
         printFunction
         jmp menuPrincipal
     derivative:
-        print newLine
         deriveFunction
         jmp menuPrincipal
     integral:
-        print prueba
+        integrateFunction
         jmp menuPrincipal
     graphFunctions:
         jmp secondMenu
     report:
-        print prueba
+        clearScreen
+        createReport
+        print repSuccess
         jmp menuPrincipal
     calculator:
-        print inputFile
+        clearScreen
+        print msgInput2
         getPath file1
         fixPath
         analisis 
+        cleanBuffer file1, SIZEOF file1, 24h
+        cleanBuffer file2, SIZEOF file2, 24h
         jmp menuPrincipal
     exit:
         mov ah, 4ch
         int 21h
     secondMenu:
+        clearScreen
         print graphMenu
         getChar 
         cmp al, '1'
@@ -126,6 +166,7 @@ main proc
         print error1
         jmp secondMenu
     plot1:
+        clearScreen
         getRange
         plotFunction
         jmp menuPrincipal
@@ -159,18 +200,29 @@ main proc
         jmp menuPrincipal
     errorOpening:
         print error5
+        cleanBuffer file1, SIZEOF file1, 24h
+        cleanBuffer file2, SIZEOF file2, 24h
         jmp menuPrincipal
     errorReading:
         print error9
+        cleanBuffer file1, SIZEOF file1, 24h
+        cleanBuffer file2, SIZEOF file2, 24h
         jmp menuPrincipal
     errorCreating:
         print error8
         jmp menuPrincipal
     errorWriting:
         print error7
+        cleanBuffer file1, SIZEOF file1, 24h
+        cleanBuffer file2, SIZEOF file2, 24h
         jmp menuPrincipal
     errorClosing:
         print error10
+        cleanBuffer file1, SIZEOF file1, 24h
+        cleanBuffer file2, SIZEOF file2, 24h
+        jmp menuPrincipal
+    errorAppending:
+        print error14
         jmp menuPrincipal
     lexicalError:
         print error11
@@ -180,6 +232,18 @@ main proc
     syntaxError:
         print error12
         jmp menuPrincipal
+    syntaxEOF:
+        print error13
+        jmp menuPrincipal
 main endp
+
+; Procedimiento para convertir de binario -> ascii
+; input : AL=binary code
+; output : AX=ASCII code
+convert proc
+    aam 
+    add ax, 3030h
+ret
+convert endp
 
 end main
